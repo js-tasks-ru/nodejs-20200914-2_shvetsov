1b152695486df4454b3deb293ff6416f819169f2
vikshv <vikshv@yandex.ru>
2020-10-08 15:37:35 +0300

    Add solution for 03-streams

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,27 @@
 const stream = require('stream');
 const os = require('os');
 
+const buffer = Symbol('length');
+
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this[buffer] = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const data = chunk.toString().split(os.EOL);
+    for (let i = 0; i < data.length - 1; i++) {
+      this.push(Buffer.from(this[buffer] + data[i]));
+      this[buffer] = '';
+    }
+    this[buffer] += data[data.length - 1];
+    callback();
   }
 
   _flush(callback) {
+    this.push(Buffer.from(this[buffer]));
+    callback();
   }
 }
 
