b39cb4a8a9c92e5ab1dc6709914adbf863f42990
vikshv <vikshv@yandex.ru>
2020-10-12 16:17:52 +0300

    Add solution for 07-mongodb-mongoose 02-rest-api

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,27 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  // await Category.create({
+  //   title: 'Category1',
+  //   subcategories: [
+  //     {
+  //       title: 'Subcategory1'
+  //     }
+  //   ]
+  // });
+
+  const result = await Category.find();
+  
+  const categories = result.map(item => ({
+    id: item.id,
+    title: item.title,
+    subcategories: item.subcategories.map(item => ({
+      id: item.id,
+      title: item.title
+    }))
+  }));
+  
+  ctx.body = {
+    categories
+  };
 };
--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,88 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+
+// http://localhost:3000/api/products?subcategory=5f844355634c34396cdb7063
+// http://localhost:3000/api/products
+// http://localhost:3000/api/products/5f844d4804d21b35fc0d4116
+
+function mapProduct(product) {
+  const {
+    id,
+    title,
+    images,
+    category,
+    subcategory,
+    price,
+    description
+  } = product;
+  
+  return {
+    id,
+    title,
+    images,
+    category,
+    subcategory,
+    price,
+    description
+  };
+}
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  // await Product.deleteMany();
+
+  // await Product.create({
+  //   title: 'Product1',
+  //   images: ['image1', 'image2'],
+  //   category: '5d20cf5bba02bff789f8e29d',
+  //   subcategory: '5f844355634c34396cdb7063',
+  //   price: 10,
+  //   description: 'Description1'
+  // });
+
+  const { subcategory } = ctx.request.query;
+
+  if (subcategory) {
+    let result;
+
+    if (mongoose.Types.ObjectId.isValid(subcategory)) {
+      result = await Product.find({ subcategory });
+    } else {
+      result = [];
+    }
+
+    const products = result.map(mapProduct);
+
+    ctx.body = {
+      products
+    };
+  } else {
+    await next();
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const result = await Product.find();
+  const products = result.map(mapProduct);
+
+  ctx.body = {
+    products
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const { id } = ctx.params;
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    const result = await Product.findOne({ _id: id });
+    if (result) {
+      const product = mapProduct(result);
+      ctx.body = {
+        product
+      };
+    } else {
+      ctx.response.status = 404;
+    }
+  } else {
+    ctx.response.status = 400;
+  }
 };
 
